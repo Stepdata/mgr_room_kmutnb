a4e7464d6b38d4ca38d4ca10c8b8a621cfe6073f
Uxcode <you@example.com>
2018-03-15 14:16:32 +0700

    Fix: cut id

--- application/views/json/debug.php
+++ application/views/json/debug.php
@@ -3,6 +3,11 @@ $status = (int)$statu_error;
 echo ("status=");
 echo ($status);
 echo (" ");
-echo ("id=");
-echo ((double)$statu_id_pass);
+echo ("id1=");
+$v1 = substr($statu_id_pass,0,7);
+$v2 = substr($statu_id_pass,7);
+echo ($v1);
+echo (" ");
+echo ("id2=");
+echo ($v2);
 ?>
