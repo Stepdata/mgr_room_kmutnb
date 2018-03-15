3c0b4ccf59b83abd1929d12ca8322498bc3f1db5
Uxcode <you@example.com>
2018-03-15 14:24:15 +0700

    cast int v1 and v2

--- application/views/json/debug.php
+++ application/views/json/debug.php
@@ -6,8 +6,8 @@ echo (" ");
 echo ("id1=");
 $v1 = substr($statu_id_pass,0,7);
 $v2 = substr($statu_id_pass,7);
-echo ($v1);
+echo ((int)$v1);
 echo (" ");
 echo ("id2=");
-echo ($v2);
+echo ((int)$v2);
 ?>
